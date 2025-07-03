b40e4f7c41f14668afbf4839e100b2df4828f6cf
prateek khened <code.prateekkhened@gmail.com>
2025-07-03 21:21:49 +0530

    set framerame to 60fps

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import pygame
 from constants import * 
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+            
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
