4e2f2ae08f92349276876b80fa4304061faa941d
prateek khened <code.prateekkhened@gmail.com>
2025-07-05 07:14:16 +0530

    grouped the updatable and drawable

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0  
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()    
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -15,8 +22,9 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
